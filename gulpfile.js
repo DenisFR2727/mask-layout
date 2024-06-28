f6cc434091abbc5868ea67b52cd51afd289fafec
Denis <dh92fr@gmail.com>
2024-06-28 17:16:30 +0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const rename = require("gulp-rename");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const cleanCSS = require("gulp-clean-css");
-const replace = require("gulp-replace");
+
 // Static server
 function server() {
   browserSync.init({
@@ -36,22 +36,3 @@ function build() {
 }
 exports.build = build;
 exports.default = gulp.parallel(watch, server, styles);
-// function build() {
-//   return gulp
-//     .src(["src/css/**/*.css", "src/*.html", "src/image/**/*", "src/icons/**/*"])
-//     .pipe(replace("src/css", "dist/css"))
-//     .pipe(replace("src/image", "dist/image"))
-//     .pipe(replace("src/icons", "dist/icons"))
-//     .pipe(gulp.dest("dist"));
-// }
-// function build() {
-//   let buildCss = gulp.src("src/css/**/*.css").pipe(gulp.dest("dist/css"));
-
-//   let buildHtml = gulp.src("src/*.html").pipe(gulp.dest("dist"));
-
-//   let buildImages = gulp.src("src/image/**/*").pipe(gulp.dest("dist/image"));
-
-//   let buildIcons = gulp.src("src/icons/**/*").pipe(gulp.dest("dist/icons"));
-
-//   return merge(buildCss, buildHtml, buildImages, buildIcons);
-// }
