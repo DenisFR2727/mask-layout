8a9e0d7da3b202fc33e958e368ac907af84d12bf
Denis <dh92fr@gmail.com>
2024-06-28 15:56:12 +0300

    mobile

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,38 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass')(require('sass'));
-const rename = require('gulp-rename');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cleanCSS = require("gulp-clean-css");
 
 // Static server
 function server() {
-    browserSync.init({
-        server: {
-            baseDir: 'src',
-        },
-    });
+  browserSync.init({
+    server: {
+      baseDir: "src",
+    },
+  });
 }
 
 function styles() {
-    return gulp
-        .src('src/sass/**/*.scss') // змініть це на ваш основний Sass файл
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(postcss([autoprefixer()]))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream());
+  return gulp
+    .src("src/sass/**/*.scss") // змініть це на ваш основний Sass файл
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
-    gulp.watch('src/sass/**/*.+(scss|sass)', styles);
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+  gulp.watch("src/sass/**/*.+(scss|sass)", styles);
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 }
-
+function build() {
+  return gulp
+    .src(["src/css/**/*.css", "src/*.html", "src/image/**/*", "src/icons/**/*"])
+    .pipe(gulp.dest("dist"));
+}
+exports.build = build;
 exports.default = gulp.parallel(watch, server, styles);
